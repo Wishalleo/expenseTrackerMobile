985714d1c9fbe8e8d923f8a6b4b5744cbac09c37
Wishal <94175604+Wishalleo@users.noreply.github.com>
2025-06-26 23:26:07 +0700

    Refactor ExpenseModel to include category and add ExpensesList for displaying expenses.

--- lib/prensention/expense_screen.dart
+++ lib/prensention/expense_screen.dart
@@ -1,10 +1,39 @@
+import 'package:expense_tracker/core/models/expense_model.dart';
+import 'package:expense_tracker/widgets/expenses_list.dart';
 import 'package:flutter/material.dart';
 
-class ExpenseScreen extends StatelessWidget {
+class ExpenseScreen extends StatefulWidget {
   const ExpenseScreen({super.key});
 
   @override
+  State<ExpenseScreen> createState() => _ExpenseScreenState();
+}
+
+final List<ExpenseModel> _registeredExpensed = [
+  ExpenseModel(
+    title: 'FLutter Course',
+    amount: 19.99,
+    date: DateTime.now(),
+    category: Category.work,
+  ),
+  ExpenseModel(
+    title: 'Cinema',
+    amount: 15.99,
+    date: DateTime.now(),
+    category: Category.leisure,
+  ),
+];
+
+class _ExpenseScreenState extends State<ExpenseScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('The Cart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpensed)),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/expenses_list.dart
@@ -0,0 +1,39 @@
+import 'package:expense_tracker/core/models/expense_model.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({required this.expenses, super.key});
+  final List<ExpenseModel> expenses;
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) {
+        final expense = expenses[index];
+        return Card(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+            child: Column(
+              children: [
+                Text(expenses[index].title),
+                Row(
+                  children: [
+                    Text('\$${expense.amount.toStringAsFixed(2)}'),
+                    Spacer(),
+                    Row(
+                      children: [
+                        Icon(Icons.alarm_rounded),
+                        SizedBox(width: 8),
+                        Text(expense.date.toString()),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
